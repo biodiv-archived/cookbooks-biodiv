f47836adee8230957c3e80fe70bd456c92cf37ad
sandeeptandekar <sandeept@strandls.com>
2014-12-09 13:17:53 +0530

    passing app name as parameter

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,12 +6,12 @@ default[:biodiv][:version]   = "master"
 default[:biodiv][:appname]   = "biodiv"
 default[:biodiv][:directory] = "/usr/local/src"
 
-default[:biodiv][:link]      = "https://codeload.github.com/strandls/biodiv/zip/#{biodiv.version}"
-default[:biodiv][:extracted] = "#{biodiv.directory}/biodiv-#{biodiv.version}"
-default[:biodiv][:war]       = "#{biodiv.extracted}/target/biodiv.war"
-default[:biodiv][:download]  = "#{biodiv.directory}/biodiv-#{biodiv.version}.zip"
+default[:biodiv][:link]      = "https://codeload.github.com/strandls/#{biodiv.appname}/zip/#{biodiv.version}"
+default[:biodiv][:extracted] = "#{biodiv.directory}/#{biodiv.appname}-#{biodiv.version}"
+default[:biodiv][:war]       = "#{biodiv.extracted}/target/#{biodiv.appname}.war"
+default[:biodiv][:download]  = "#{biodiv.directory}/#{biodiv.appname}-#{biodiv.version}.zip"
 
-default[:biodiv][:home] = "/usr/local/biodiv"
+default[:biodiv][:home] = "/usr/local/#{biodiv.appname}"
 
 
 # mail server
