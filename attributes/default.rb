0495def5dc1c78ec6e14c44bb62cfeb90bef98c4
sandeeptandekar <sandeept@strandls.com>
2014-12-09 17:28:37 +0530

    minor

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,13 @@ expand!
 
 default[:biodiv][:version]   = "master"
 default[:biodiv][:appname]   = "biodiv"
+default[:biodiv][:repository]   = "biodiv"
 default[:biodiv][:directory] = "/usr/local/src"
 
-default[:biodiv][:link]      = "https://codeload.github.com/strandls/biodiv/zip/#{biodiv.version}"
+default[:biodiv][:link]      = "https://codeload.github.com/strandls/#{biodiv.repository}/zip/#{biodiv.version}"
 default[:biodiv][:extracted] = "#{biodiv.directory}/biodiv-#{biodiv.version}"
 default[:biodiv][:war]       = "#{biodiv.extracted}/target/biodiv.war"
-default[:biodiv][:download]  = "#{biodiv.directory}/#{biodiv.appname}-#{biodiv.version}.zip"
+default[:biodiv][:download]  = "#{biodiv.directory}/#{biodiv.repository}-#{biodiv.version}.zip"
 
 default[:biodiv][:home] = "/usr/local/biodiv"
 
